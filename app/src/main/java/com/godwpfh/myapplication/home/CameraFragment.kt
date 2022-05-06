66bf91e1122e4df040114327cb312002c32a45f5
wlwpfh <0112jyoung@naver.com>
2022-05-06 15:56:34 +0900

    Add NestedScrollableHost, glide etc

--- app/src/main/java/com/godwpfh/myapplication/home/CameraFragment.kt
+++ app/src/main/java/com/godwpfh/myapplication/home/CameraFragment.kt
@@ -1,32 +1,59 @@
 package com.godwpfh.myapplication.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.godwpfh.myapplication.R
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.godwpfh.myapplication.databinding.FragmentCameraBinding
-import com.godwpfh.myapplication.databinding.FragmentProfileBinding
 
 
 class CameraFragment : Fragment() {
-    private var _binding : FragmentCameraBinding?=null
-    private val binding  get() = _binding!!
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding=FragmentCameraBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCameraBinding.inflate(layoutInflater, container, false)
 
+        initCameraButton()
+        setPhotoFromGallery()
         return binding.root
+    }
+
+    private fun initCameraButton() {
+        binding.fragmentCameraButton.setOnClickListener {
+            val intent = Intent()
+            intent.apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+            }
+            activityResultLauncher.launch(intent)
+        }
+    }
+
+    private fun setPhotoFromGallery() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                Glide.with(this)
+                    .load(result.data?.data)
+                    .into(binding.fragmentCameraImageview)
+
+            }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 
 }
--- app/src/main/java/com/godwpfh/myapplication/home/ProfileFragment.kt
+++ app/src/main/java/com/godwpfh/myapplication/home/ProfileFragment.kt
@@ -1,59 +1,64 @@
 package com.godwpfh.myapplication.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
 import com.godwpfh.myapplication.R
 import com.godwpfh.myapplication.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
-    private var _binding : FragmentProfileBinding?=null
-    private val binding  get() = _binding!!
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
-    private  val followFragment = FollowFragment()
+    private val followFragment = FollowFragment()
     private val reposFragment = ReposFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=FragmentProfileBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
 
         initTransaction()
-        initFollowButton()
-        initReposButton()
+
 
         return binding.root
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 
-    private fun initFollowButton(){
+
+    private fun initTransaction() {
+        //처음에 보여질 때 follow로 뜨게
+        childFragmentManager.beginTransaction()
+            .add(R.id.profile_fragmentview, followFragment)
+            .commit()
+
+        binding.homeFollowBtn.isSelected = true
+        binding.homeReposBtn.isSelected = false
+
+
         binding.homeFollowBtn.setOnClickListener {
             childFragmentManager.beginTransaction()
                 .replace(R.id.profile_fragmentview, followFragment)
                 .commit()
-        }
-    }
 
-    private fun initReposButton(){
+            binding.homeFollowBtn.isSelected = true
+            binding.homeReposBtn.isSelected = false
+        }
+
         binding.homeReposBtn.setOnClickListener {
             childFragmentManager.beginTransaction()
                 .replace(R.id.profile_fragmentview, reposFragment)
                 .commit()
+            binding.homeReposBtn.isSelected = true
+            binding.homeFollowBtn.isSelected = false
         }
-    }
-
-    private fun initTransaction(){
-        //처음에 보여질 때 뜨게
-        childFragmentManager.beginTransaction()
-            .add(R.id.profile_fragmentview, followFragment)
-            .commit()
     }
 }
